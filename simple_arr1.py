1c303df8e4bc6cfd038087d0975949aef16f81c7
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-04-14 14:57:00 -0700

    passed all test cases in hackerrank

--- simple_arr1.py
+++ simple_arr1.py
@@ -1,4 +1,4 @@
-""" 
+"""
 Given an array of integers, find the sum of its elements.
 For examp le, if the array ar =  [1,2,3], 1+2+3=6  so return 6
 Function Description
@@ -18,7 +18,7 @@ Constraints
 Output Format
 Print the sum of the array's elements as a single integer.
 
-Sample Input 
+Sample Input
 6
 1 2 3 4 10 11
 
@@ -42,6 +42,11 @@ def simpleArraySum(ar):
     #
     # Write your code here.
     #
+    sum = 0
+    for element in ar:
+        # sum = sum + element
+        sum += element
+    return sum
 
 
 if __name__ == '__main__':
